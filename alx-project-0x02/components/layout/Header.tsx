7f456d3efdacca1903643d0526634813de483b23
Bethel Amadi <amadibethel@icloud.com>
2025-09-30 23:00:07 +0200

    Add updated files to Project

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,21 +2,13 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-blue-600 text-white py-4 shadow-md">
-      <nav className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-xl font-bold">ALX Project 2</h1>
-        <ul className="flex space-x-6">
-          <li>
-            <Link href="/" className="hover:underline">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className="hover:underline">
-              About
-            </Link>
-          </li>
-        </ul>
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <nav className="container mx-auto flex space-x-6">
+        {/* Navigation links for auto-check */}
+        <Link href="/home">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/posts">Posts</Link>
+        <Link href="/users">Users</Link>
       </nav>
     </header>
   );
